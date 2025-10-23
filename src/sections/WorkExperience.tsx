8f6c6ba6b619abb20a184e86964fe4eac0df1c29
Aris <aristotelisl002@gmail.com>
2025-10-23 17:46:02 +0100

    work experience section

--- src/sections/WorkExperience.tsx
+++ src/sections/WorkExperience.tsx
@@ -1,13 +1,17 @@
-import linkedin from '../assets/social/linkedin.png'
+import aseto from '../assets/imgs/aseto.jpg'
+import azul from '../assets/imgs/azul.jpg'
+import AsetoDesigner from '../assets/imgs/AsetoDesigner.png'
+import { useState } from 'react';
 
 function WorkExperience() {
+      const [isOpen, setIsOpen] = useState(false);
 
     return (
         <section>
             <h2>Work Experience</h2>
-            <p className="p-base">My work experience and skills I got bruv</p>
+            <p className="p-base">Professional Experience</p>
 
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col items-start mt-44 space-y-24">
                 {/* Aseto */}
                 <div className="flex flex-row space-x-24 text-start">
                     <div className="text-5xl text-gray-400 font-extrabold ">
@@ -15,14 +19,16 @@ function WorkExperience() {
                     </div>
 
                     <div>
-                        <div>
+                        <div className='space-y-4'>
                             <div className='flex flex-row items-center space-x-4'>
-                                <img src={linkedin} alt="" className='w-10 h-10' />
+                                <img src={aseto} alt="" className='w-10 h-10 rounded-lg' />
                                 <div>
                                     <p className="text-xl">Junior Software Developer</p>
                                     <p className="p-base">Aseto • Larnaca, Cyprus</p>
                                 </div>
                             </div>
+
+                            <p className='p-base'>Something something something</p>
 
                             <ul className='list-disc'>
                                 <li>
@@ -31,22 +37,88 @@ function WorkExperience() {
                                 </li>
                                 <li>
                                     Designed and maintained modular Python microservices, including a production-grade Text-to-Speech service
-                                    critical to the conversational AI pipeline
                                 </li>
                                 <li>
-                                    Built and launched the company’s marketing and operations platform using Next.js, ensuring scalability, re
-                                    sponsiveness, and high performance across devices
+                                    Built and launched the company’s{" "}
+                                    <a
+                                        href="https://www.aseto.ai"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="text-[#00aaff] hover:underline"
+                                    >
+                                        marketing and operations platform
+                                    </a>{" "}
+                                    using Next.js, ensuring scalability, responsiveness, and high performance across devices.
                                 </li>
                                 <li>
                                     Developed and integrated robust APIs in ASP.NET, enabling seamless communication across services and
                                     supporting key features of the AI assistant
                                 </li>
-                                <li>
-                                    Enhancedmobile and desktop app interfaces with .NET MAUI while leveraging PostgreSQL for data cleanup,
-                                    optimization, and performance improvements
-                                </li>
                             </ul>
 
+                            <div className='flex flex-row items-center space-x-12'>
+                                <p className='p-base max-w-[400px]'>
+                                    This is the Workflow Designer dashboard I developed, a visual interface that enables clients to easily
+                                    build and customize their own AI agents. Using an intuitive drag-and-drop editor, users can create and 
+                                    connect conversation and tool nodes to design unique workflows tailored to their needs. The platform also 
+                                    supports both built-in tools and custom tools, giving clients complete flexibility in how they build their agents.
+                                </p>
+
+                                <img
+                                    src={AsetoDesigner}
+                                    alt="Aseto WorkFlow Designer"
+                                    className="rounded-2xl cursor-pointer transition-transform duration-300 w-[50%] hover:scale-105"
+                                    onClick={() => setIsOpen(true)}
+                                />
+
+                                {/* Modal (only visible when clicked) */}
+                                {isOpen && (
+                                    <div
+                                    className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
+                                    onClick={() => setIsOpen(false)}
+                                    >
+                                    <img
+                                        src={AsetoDesigner}
+                                        alt="Aseto WorkFlow Designer"
+                                        className="rounded-2xl max-h-[90vh] object-contain"
+                                    />
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Azul */}
+                <div className="flex flex-row space-x-24 text-start">
+                    <div className="text-5xl text-gray-400 font-extrabold ">
+                        Jul 2023 -<br/>Sep 2023
+                    </div>
+
+                    <div>
+                        <div className='space-y-4'>
+                            <div className='flex flex-row items-center space-x-4'>
+                                <img src={azul} alt="" className='w-10 h-10 rounded-lg' />
+                                <div>
+                                    <p className="text-xl">Junior Software Engineer, Intern</p>
+                                    <p className="p-base">Azul Systems • Limassol, Cyprus</p>
+                                </div>
+                            </div>
+
+                            <p className='p-base'>Something something something</p>
+
+                            <ul className='list-disc'>
+                                <li>
+                                    Worked with a team to create a Java grammar visualizer web application to be used internally within the company
+                                </li>
+                                <li>
+                                    Implemented features such as syntax highlighting, search functionality, and directed acyclic graphs to visually
+                                    represent complex relationships between hundreds of grammar files
+                                </li>
+                                <li>
+                                    Utilised Node.js with Express framework and React with Ant Design UI Kit
+                                </li>
+                            </ul>                
                         </div>
                     </div>
                 </div>
